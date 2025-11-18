130e4c7256f2cbc1feef79353505e42f5cc48e9e
OAB Seccional do Maranhão <informatica@oabma.org.br>
2025-11-18 18:05:55 -0300

    Instalacao e configuracao Shadcn e Fontes

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <main>
+      <div className="font-bold font-calsans text-6xl">Hero Public</div>
+      <Button className="text-2xl">Entrar</Button>
+    </main>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import './globals.css'
+import './styles/globals.css'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR">
-      <body>{children}</body>
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className="font-geist antialiased">{children}</body>
     </html>
   )
 }
